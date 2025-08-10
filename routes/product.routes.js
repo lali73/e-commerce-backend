807db15af0527072823d337a197219a547889d9c
YourGitHubUsername <your@email.com>
2025-08-10 18:12:56 +0300

    singleProduct Functionality added + coros for the admin portal

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import{allProducts,createProduct,singleProduct,updateProduct,deleteProduct} from "../controllers/product.controller.js";
+import{allProducts,createProduct,singleProduct,} from "../controllers/product.controller.js";
 import upload from '../utils/multer.js'
 
 
@@ -8,7 +8,7 @@ const productRouter = Router();
 productRouter.post('/createProduct',upload.array('images',5),createProduct)
 productRouter.get('/allProducts',allProducts)
 productRouter.get('/singleProduct/:id',singleProduct)
-productRouter.patch('/updateProduct/:id',updateProduct)
-productRouter.delete('/deleteProduct/:id',deleteProduct)
+// productRouter.patch('/updateProduct/:id',updateProduct)
+// productRouter.delete('/deleteProduct/:id',deleteProduct)
 
 export default productRouter;
