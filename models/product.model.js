011c400fbde65649e558f8b96fc1d1cb114c6fca
YourGitHubUsername <your@email.com>
2025-08-10 18:32:50 +0300

    edited the creatProduct funtionality to include vendorId

--- models/product.model.js
+++ models/product.model.js
@@ -10,6 +10,7 @@ const ProductSchema = new mongoose.Schema(
 
         category:{type:String,required:true,},
         stock:{type:Number,required:true,min:[0,'Stock must be greater than 0'],
+         vendorId:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true,},
         validate:{validator:Number.isInteger,message:'Stock must be an Integer'},},
 
 },{timestamps:true});
