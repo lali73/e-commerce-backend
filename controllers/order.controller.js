21b4295f9fce8cc69ff2fbb40c281ec5aba99606
YourGitHubUsername <your@email.com>
2025-09-04 10:31:09 +0300

    addinitiatePayment ,paymentWebhook functionality with therir route and enviroment variables result in integrating with chapa

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -1,6 +1,10 @@
 import Order from '../models/order.model.js'
 import Product from '../models/product.model.js'
 import crypto from 'crypto';
+import {config} from 'dotenv'
+import axios from 'axios'
+
+config();
 
 export const creatOrder = async (req,res,next)=>{
    try{
@@ -25,11 +29,85 @@ export const creatOrder = async (req,res,next)=>{
 
        const gateway = "Chapa"
        const order = await Order.create({userId,items,orderId,gateway,amount})
+
+
        res.status(201).json({success:true,message:'Order successfully created',order})
 
    }
    catch(error){
     next(error)
    }
+}
+export const initiatePayment = async (req,res,next)=>{
+   try{
+       const {orderId} = req.body;
+       const order = await Order.findOne({orderId:orderId})
+       if(!order){
+           return res.status(400).send({message:'Order not found.'});
+       }
+       const url = "https://api.chapa.co/v1/transaction/initialize"
+       const paymentPayload = {
+           amount:order.amount.toString(),
+           tx_ref: order.orderId,
+           callback_url:" https://88ac92d86878.ngrok-free.app/api/v1/order/paymentWebhook",
+           currency:"ETB",
 
+           customization:{
+               title:"payment for ",
+               description:"payment description",
+           }
+       }
+       const headers = {
+           "Authorization": `Bearer ${process.env.CHAPA_SECRET_KEY}`,
+           "content-type": "application/json",
+       }
+
+
+
+       const response = await axios.post(url,paymentPayload,{headers:headers})
+        const data = response.data
+       const checkoutUrl = data.data.checkout_url;
+       order.paymentSession = {
+           paymentUrl:checkoutUrl,
+           txRef:order.orderId,
+       };
+       await order.save();
+       res.status(200).json({success:true,paymentUrl:checkoutUrl});
+   }
+   catch(error){
+       if (error.response) {
+           console.error("Chapa error:", error.response.data);
+           return res.status(error.response.status).json(error.response.data);
+       } else {
+           console.error("Error:", error.message);
+           next(error);
+       }
+   }
+}
+export const paymentWebhook = async (req,res,next)=>{
+    try{
+
+        const {trx_ref,status}=req.body;
+        const order = await Order.findOne({orderId:trx_ref})
+        if(!order){
+            return res.status(400).send({message:'Order not found.'});
+        }
+        if(status==="success"){
+        const request =   await axios.get(`https://api.chapa.co/v1/transaction/verify/${trx_ref}`,{headers:{Authorization: `Bearer ${process.env.CHAPA_SECRET_KEY}`}})
+            const verifyData = request.data;
+        if (verifyData.status === "success"&&verifyData.data.status === "success"){
+            order.status="Paid";
+            order.transactionId=verifyData.data.reference
+            await order.save();
+        }
+        else{
+            order.status="Failed"
+           await order.save();
+        }
+        }
+        res.status(200).json({success:true,message:"Webhook received."});
+    }
+    catch(error){
+        next(error)
+    }
 }
--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,9 +1,11 @@
 import {Router} from 'express'
-import {creatOrder} from "../controllers/order.controller.js";
+import {creatOrder, initiatePayment, paymentWebhook} from "../controllers/order.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const orderRouter = new Router();
 
-orderRouter.post('/createOrder',authMiddleware,creatOrder)
+orderRouter.post('/createOrder',authMiddleware, creatOrder)
+orderRouter.post('/initiatePayment',initiatePayment)
+orderRouter.get('/paymentWebhook',paymentWebhook)
 
 export default orderRouter;
