8f04b10be74fed435205c2c468c6d9898ed5c378
YourGitHubUsername <your@email.com>
2025-09-24 10:38:43 +0300

    add routes of verify order

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,5 +1,12 @@
 import {Router} from 'express'
-import {creatOrder, deliverOrder, initiatePayment, myOrders, paymentWebhook} from "../controllers/order.controller.js";
+import {
+    creatOrder,
+    deliverOrder,
+    initiatePayment,
+    myOrders,
+    paymentWebhook,
+    verifyOrder
+} from "../controllers/order.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 
 const orderRouter = new Router();
@@ -9,5 +16,6 @@ orderRouter.post('/initiatePayment',initiatePayment)
 orderRouter.get('/paymentWebhook',paymentWebhook)
 orderRouter.get('/myOrders',authMiddleware,myOrders)
 orderRouter.post('/deliverOrder',authMiddleware,deliverOrder)
+orderRouter.post('verifyOrder',authMiddleware,verifyOrder)
 
 export default orderRouter;
